8ed69dbaa1a4459adc9796f749da3c60edb542dc
Janice Chan <janice.chan1288@gmail.com>
2022-06-21 07:09:45 -0400

    commit tree c0cea57a83feb4178d4941597185cae387a5a47e

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,2 +1,19 @@
-require_relative './dance_module'
-require_relative './meta_dancing_module'
+# require_relative './dance_module'
+# require_relative './meta_dancing_module'
+require_relative './fancy_dance.rb'
+
+class Kid
+    # To lend our two classes all of the methods of the Dance module use the include keyword
+    # include Dance
+    # extend MetaDancing
+
+    #using namespacing modules
+    extend FancyDance::ClassMethods
+    include FancyDance::InstanceMethods
+
+    attr_accessor :name
+
+    def initialize(name)
+    @name = name
+    end
+end
